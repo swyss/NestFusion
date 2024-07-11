985582110f06e2a7d10c7a84fa68bd0017a458a9
s.wyss <stefan.arnold.wyss@gmail.com>
2024-07-12 00:05:52 +0200

    feat(frontend): adjust auth-routing ->Router.beforeEach: authRequired

--- src/frontend/webapp/src/router/routes.js
+++ src/frontend/webapp/src/router/routes.js
@@ -27,4 +27,5 @@ const routes = [
   }
 ]
 
+
 export default routes
--- src/frontend/webapp/src/stores/user-auth.js
+++ src/frontend/webapp/src/stores/user-auth.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     User: null,
-    loggedIn: false
+    loggedIn: true // change to false
   }),
 
   getters: {
