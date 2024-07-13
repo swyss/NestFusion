63fb10668731c9fe6ed49befeade000fc7df839f
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-07-13 22:51:02 +0200

    Fixed indents

--- src/backend/web-service-gin/repositories/TaskRepository.go
+++ src/backend/web-service-gin/repositories/TaskRepository.go
@@ -8,25 +8,25 @@ import (
 
 type TaskRepository struct{}
 
-func NewTaskRepository() *TaskRepository{
-  return &TaskRepository{}
+func NewTaskRepository() *TaskRepository {
+	return &TaskRepository{}
 }
 
 var tasks = []models.Task{
-  {ID: "1", Name: "Task One", Created: time.Now(), Due: time.Now().Add(24 * time.Hour), Finished: false},
-  {ID: "2", Name: "Task Two", Created: time.Now(), Due: time.Now().Add(48 * time.Hour), Finished: true},
+	{ID: "1", Name: "Task One", Created: time.Now(), Due: time.Now().Add(24 * time.Hour), Finished: false},
+	{ID: "2", Name: "Task Two", Created: time.Now(), Due: time.Now().Add(48 * time.Hour), Finished: true},
 	{ID: "3", Name: "Task Three", Created: time.Now(), Due: time.Now().Add(72 * time.Hour), Finished: false},
 }
 
-func (repo *TaskRepository) GetTasks() ([]models.Task)  {
-  return tasks
+func (repo *TaskRepository) GetTasks() []models.Task {
+	return tasks
 }
 
-func (repo *TaskRepository) GetTaskById(id string) (*models.Task, error){
-  for _, task := range tasks {
-        if task.ID == id {
-            return &task, nil
-        }
-    }
-    return nil, errors.New("Task not found")
+func (repo *TaskRepository) GetTaskById(id string) (*models.Task, error) {
+	for _, task := range tasks {
+		if task.ID == id {
+			return &task, nil
+		}
+	}
+	return nil, errors.New("Task not found")
 }
