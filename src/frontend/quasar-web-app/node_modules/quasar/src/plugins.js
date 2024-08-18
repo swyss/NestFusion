9718d80c13c8495feb1d036aa16664b6d543d7d6
s.wyss <stefan.arnold.wyss@gmail.com>
2024-08-18 19:00:31 +0200

    backend merge

---
+++ src/frontend/quasar-web-app/node_modules/quasar/src/plugins.js
@@ -0,0 +1,37 @@
+import AddressbarColor from './plugins/addressbar/AddressbarColor.js'
+import AppFullscreen from './plugins/app-fullscreen/AppFullscreen.js'
+import AppVisibility from './plugins/app-visibility/AppVisibility.js'
+import BottomSheet from './plugins/bottom-sheet/BottomSheet.js'
+import Cookies from './plugins/cookies/Cookies.js'
+import Dark from './plugins/dark/Dark.js'
+import Dialog from './plugins/dialog/Dialog.js'
+import IconSet from './plugins/icon-set/IconSet.js'
+import Lang from './plugins/lang/Lang.js'
+import Loading from './plugins/loading/Loading.js'
+import LoadingBar from './plugins/loading-bar/LoadingBar.js'
+import Meta from './plugins/meta/Meta.js'
+import Notify from './plugins/notify/Notify.js'
+import Platform from './plugins/platform/Platform.js'
+import Screen from './plugins/screen/Screen.js'
+import LocalStorage from './plugins/storage/LocalStorage.js'
+import SessionStorage from './plugins/storage/SessionStorage.js'
+
+export {
+  AddressbarColor,
+  AppFullscreen,
+  AppVisibility,
+  BottomSheet,
+  Cookies,
+  Dark,
+  Dialog,
+  IconSet,
+  Lang,
+  Loading,
+  LoadingBar,
+  Meta,
+  Notify,
+  Platform,
+  Screen,
+  LocalStorage,
+  SessionStorage
+}
