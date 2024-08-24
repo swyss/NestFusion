6bc2a9d4d300e0f354e35f57ebbb630f3a068850
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-08-24 20:37:53 +0200

    Finished task model

--- src/backend/go-crud-api/internal/models/task.go
+++ src/backend/go-crud-api/internal/models/task.go
@@ -1,8 +1,12 @@
 
 package models
+import "time"
 
 type Task struct {
 	ID       uint   `json:"id" gorm:"primary_key;autoIncrement"` // Primary key with auto-increment.
 	Name     string `json:"name" gorm:"size:100"`                // Limits the name field to 100 characters for efficient database usage.
+  Created time.Time `json:"created" gorm:"autoCreateTime"` // autoCreateTime sets the current timestamp when the record is created
+	Due     time.Time `json:"due"`
 	IsFinished bool   `json:"isFinished" gorm:"default:false"`       // Sets default value to true, indicating the user is active by default.
 }
+
